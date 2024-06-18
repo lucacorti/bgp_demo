bd6e2a26713beffd4bf14b53fb4584df31a7ae28
Luca Corti <luca@fantacast.it>
2024-06-19 00:57:12 +0200

    Fix credo strict

--- lib/bgp_demo_web/components/core_components.ex
+++ lib/bgp_demo_web/components/core_components.ex
@@ -16,7 +16,9 @@ defmodule BGPDemoWeb.CoreComponents do
   """
   use Phoenix.Component
 
+  alias Phoenix.HTML.Form
   alias Phoenix.LiveView.JS
+
   import BGPDemoWeb.Gettext
 
   @doc """
@@ -305,7 +307,7 @@ defmodule BGPDemoWeb.CoreComponents do
   def input(%{type: "checkbox"} = assigns) do
     assigns =
       assign_new(assigns, :checked, fn ->
-        Phoenix.HTML.Form.normalize_value("checkbox", assigns[:value])
+        Form.normalize_value("checkbox", assigns[:value])
       end)
 
     ~H"""
